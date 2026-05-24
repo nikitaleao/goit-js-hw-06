9c6fe389a452697ff3b92539d6e143f576367b88
Nikita <nikita.leao.ua@gmail.com>
2026-05-25 00:01:58 +0300

    edit hw 1

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,19 @@
 class StringBuilder {
+  #value;
   constructor(value) {
-    this.value = value;
+    this.#value = value;
   }
   getValue() {
-    return this.value;
+    return this.#value;
   }
   padStart(str) {
-    this.value = str + this.value;
+    this.#value = str + this.#value;
   }
   padEnd(str) {
-    this.value = this.value + str;
+    this.#value = this.#value + str;
   }
   padBoth(str) {
-    this.value = str + this.value + str;
+    this.#value = str + this.#value + str;
   }
 }
 const builder = new StringBuilder('.');
